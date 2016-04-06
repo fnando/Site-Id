d47574eaa0aedb1e5a1eaf45b5430fda539c9195
Nando Vieira <fnando.vieira@gmail.com>
2016-04-06 08:32:02 -0300

    Retry just 3 times.

--- inject.js
+++ inject.js
@@ -1,12 +1,14 @@
-(function siteId(){
+(function siteId(tries) {
   var hostname = location.hostname.replace(/[^a-z0-9]/gm, "_");
   var className = "id__" + hostname;
   var html = document.documentElement;
 
-  if (!html) {
-    setTimeout(siteId, 300);
+  if (!html && tries < 3) {
+    setTimeout(function() {
+      siteId(tries + 1);
+    }, 300);
   } else {
     html.classList.add(className);
     html.dataset.siteId = hostname;
   }
-}());
+}(0));
